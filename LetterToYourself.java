3d06e8ee4e0e9d0b12fd4f12e36f4bafcb239d0a
13badjei <13badjei@gmail.com>
2020-05-16 13:25:12 +0100

    Add files via upload

---
+++ LetterToYourself.java
@@ -0,0 +1,16 @@
+public class LetterToYourself {
+
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              Benjamin Adjei             |");
+        System.out.println("|                              108 Woodisde Road          |");
+        System.out.println("|                              London, N22 5HS	      |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
---
+++ YourInitials.java
@@ -0,0 +1,18 @@
+/**
+ * Your Initials
+ * http://programmingbydoing.com/a/your-initials.html
+ * Display your initials on the screen in block letters as shown.
+ */
+
+public class YourInitials {
+
+    public static void main(String[] args) {
+        System.out.println("BBBB    A  ");
+        System.out.println("B   B  A A ");
+        System.out.println("B   B A   A");
+        System.out.println("BBBB  AAAAA");
+        System.out.println("B   B A    A");
+        System.out.println("B   B A    A");
+        System.out.println("BBBB  A    A");
+    }
+}
